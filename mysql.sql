2193b66b05afde945f974cf6dad4e823f28b8cc0
Neo Wei Thiam <wei-thiam.neo@leica-microsystems.com>
2023-12-29 17:25:55 +0800

    update 4

--- mysql.sql
+++ mysql.sql
@@ -2,4 +2,3 @@ CREATE USER 'nextclouduser'@'localhost' IDENTIFIED BY 'Thiam';
 CREATE DATABASE IF NOT EXISTS nextcloud CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 GRANT ALL PRIVILEGES ON nextcloud.* TO 'nextclouduser'@'localhost';
 FLUSH PRIVILEGES;
-EXIT;
