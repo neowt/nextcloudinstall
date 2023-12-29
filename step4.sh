3dfcbf2605957a2dae52a61e477bf6dca29ee624
Neo Wei Thiam <wei-thiam.neo@leica-microsystems.com>
2023-12-29 17:36:12 +0800

    update 6

--- step4.sh
+++ step4.sh
@@ -2,3 +2,14 @@ cd /var/www/html/nextcloud
 sudo -u www-data php occ  maintenance:install \
         --database='mysql' --database-name='nextcloud' \
         --database-user='nextclouduser' --database-pass='Thiam'
+
+echo "In file /etc/apache2/ports.conf change"
+echo "          Listen 80 "
+echo "            to "
+echo "            Listen 0.0.0.0:80"
+
+
+echo "edit /var/www/html/nextcloud/config/config.php"
+echo " 'trusted_domains' =>"
+echo " 0 => 'localhost',"
+echo " 1 => '192.168.1.*',"
