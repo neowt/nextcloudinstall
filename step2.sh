1dd0f63818dea611ee93a715539147bc6b266f70
Neo Wei Thiam <wei-thiam.neo@leica-microsystems.com>
2023-12-29 17:00:52 +0800

    update 2

--- step2.sh
+++ step2.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 sudo apt install apache2 mariadb-server libapache2-mod-php php-gd php-mysql \
-php-curl php-mbstring php-intl php-gmp php-bcmath php-xml php-imagick php-zip -y
+php-curl php-mbstring php-intl php-gmp php-bcmath php-xml php-imagick php-zip unzip -y
+reboot
